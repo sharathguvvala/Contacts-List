bcd55464cac21cab3dc1ed455a310a6bb54d034f
sharathguvvala <guvvalasharath@gmail.com>
2021-05-14 02:54:11 +0530

    modified index file

--- index.js
+++ index.js
@@ -4,39 +4,13 @@ const port = 8000;
 const db = require('./config/mongoose')
 const Contact = require('./models/contact')
 const app = express();
-/*app.get('/home',function(req,res){
-    res.send("Cool! it's running")
-})
-app.get('/about',function(req,res){
-    res.send("<h1>This is about page</h1>")
-})
-app.get('/contact',function(req,res){
-    res.send("<h2>This is contact page</h2>")
-})*/
+
 app.set('view engine','ejs')
 app.set('views',path.join(__dirname,'views'))
 app.use(express.urlencoded())
-/*app.get('/',function(req,res){
-    return res.render('home')
-})*/
-/*app.get('/',function(req,res){
-    return res.render('home',{title:"My Contacts List"})
-})*/
+
 app.use(express.static('assets'))
-var ContactList = [
-    {
-        name:'Sharath',
-        phone:'7093618330'
-    },
-    {
-        name:'Rahul',
-        phone:'9494056236'
-    },
-    {
-        name:'Rohan',
-        phone:'9346756765'
-    }
-]
+
 app.get('/',function(req,res){
 
     Contact.find({},function(err,contacts){
@@ -50,10 +24,6 @@ app.get('/',function(req,res){
         })
     })
 
-    /*return res.render('contacts',{
-        title:"Practice Page",
-        Contact_List:ContactList
-    })*/
 })
 
 app.get('/practice',function(req,res){
@@ -62,25 +32,7 @@ app.get('/practice',function(req,res){
     })
 })
 
-/*app.post('/create-contact',function(req,res){
-    return res.redirect('/practice')
-})*/
-/*app.use(function(req,res,next){
-    req.myname="sharath"
-    next()
-})*/
 app.post('/create-contact', function(req,res){
-    //console.log(req.body)
-    //console.log(req.myname)
-    //console.log(req.body.name)
-    //console.log(req.body.phone)
-    /*ContactList.push({
-        name:req.body.name,
-        phone:req.body.phone
-    })*/
-    //ContactList.push(req.body)
-
-
     Contact.create({
         name:req.body.name,
         phone:req.body.phone
@@ -89,31 +41,11 @@ app.post('/create-contact', function(req,res){
             console.log('error in creating a new contact')
             return
         }
-        //console.log('$$$$$',newcontact)
         return res.redirect('back')
     })
-
-
-    //return res.redirect('/')
-    //return res.redirect('back')
 })
 
-/*app.get('/delete-contact/:phone',function(req,res){
-    console.log(req.params)
-    let phone = req.params.phone
-})*/
 app.get('/delete-contact',function(req,res){
-    //console.log(req.query)
-    //let phone = req.query.phone
-    //console.log(phone)
-    //let delindex = ContactList.findIndex(contact => contact.phone == phone)
-    //console.log(delindex)
-    /*if(delindex != -1){
-        ContactList.splice(delindex,1)
-    }*/
-    //return res.redirect('back')
-
-
     let id =req.query.id;
     Contact.findByIdAndDelete(id,function(err){
         if(err){
@@ -122,13 +54,10 @@ app.get('/delete-contact',function(req,res){
         }
         return res.redirect('back')
     })
-
-
 })
 app.listen(port,function(err){
     if(err){
         console.log('Error:',err)
     }
-    //console.log(__dirname)
     console.log('My express is running on port:',port);
 })
